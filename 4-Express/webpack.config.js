7fc14c9b682ac7e96662ac916a4ae2bfee19f502
harel-hilu <harelh@wix.com>
2020-10-29 16:38:09 +0200

    w

--- 4-Express/webpack.config.js
+++ 4-Express/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: '/Users/harelh/Documents/Code/todo-onboarding/todo/4-Express/src/index.js',
   context: '/Users/harelh/Documents/Code/todo-onboarding/todo/4-Express/src/',
   devtool: 'source-map',
   watch: true,
