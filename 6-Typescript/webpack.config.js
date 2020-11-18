0fe14b74a04ba2921213ee63283052209c6787bc
harel-hilu <harelh@wix.com>
2020-11-18 11:21:36 +0200

    m

--- 6-Typescript/webpack.config.js
+++ 6-Typescript/webpack.config.js
@@ -18,6 +18,6 @@ module.exports = {
   },
   output: {
     filename: 'client.bundle.js',
-    path: path.resolve(__dirname, '../todo-server/public'),
+    path: path.resolve(__dirname, '../server/public'),
   },
 };
