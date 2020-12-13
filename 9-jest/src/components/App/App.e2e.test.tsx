c8036411b0d1f02513bbb41e43648634c4394076
harel-hilu <harelh@wix.com>
2020-12-13 18:15:25 +0200

    TDD

--- 9-jest/src/components/App/App.e2e.test.tsx
+++ 9-jest/src/components/App/App.e2e.test.tsx
@@ -15,21 +15,50 @@ beforeAll(async () => {
   
 });
 
-test("check the title", async () => {
+test("check sanity and the title", async () => {
 await page.waitForSelector("#root");
 const header = await page.$eval("#title", e => e.innerHTML);
   expect(header).toBe(`Create your first task`);
 });
 
-test("add task", async () => {
-    await page.waitForSelector("#root");
-    await page.type("#addTaskInput", "First Task!");
-    await page.click("#addTaskButton");
-    await page.waitForTimeout(100);
+describe("CRUD tasks", () => {
+    test("add task", async () => {
+        await page.waitForSelector("#root");
+        await page.type("#addTaskInput", "First task");
+        await page.click("#addTaskButton");
+        await page.waitForTimeout(100);
+        
+        const header = await page.$eval("#title", e => e.innerHTML);
+        expect(header).toBe(`0/1 tasks`);
     
-    const header = await page.$eval("#title", e => e.innerHTML);
-    expect(header).toBe(`0/1 tasks`);
-  });
+        const taskContent = await page.$eval('[data-hook="taskLabel"]', e => e.innerHTML);
+        expect(taskContent).toBe("First task");
+        
+        const numOfTasks = await page.$$eval('[data-hook="taskLabel"]', e => e.length);
+        expect(numOfTasks).toBe(1);
+    });
+
+    test("update task", async () => {
+        await page.click('[data-hook="taskLabel"]');
+        await page.type('[data-hook="taskInput"]', " ever");
+        await page.click('[data-hook="taskCheckbox"]');
+        await page.waitForTimeout(100);
+
+        const taskLabel = await page.$eval('[data-hook="taskLabel"]', e => e.textContent);
+        expect(taskLabel).toBe("First task ever");
+
+        const numOfTasks = await page.$$eval('[data-hook="taskLabel"]', e => e.length);
+        expect(numOfTasks).toBe(1);
+    });
+        
+    test("delete task", async () => {
+        await page.click('[data-hook="taskDeleteButton"]');
+        await page.waitForTimeout(100);
+
+        const numOfTasks = await page.$$eval('[data-hook="taskLabel"]', e => e.length);
+        expect(numOfTasks).toBe(0);
+    });
+});
 
 afterAll(() => {
   browser.close();
